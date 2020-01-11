638c7d3020c20c6321ee15f38ff772227b4f5fa7
Dong-jin Ahn <adoji92@gmail.com>
2020-01-11 23:18:16 +0900

    Install koa-router

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import Koa from 'koa';
+import Router from 'koa-router';
 
 const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  ctx.body = '흠zz';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000, () => {
-  console.log('hello');
+  console.log('listening on port 3000');
 });
