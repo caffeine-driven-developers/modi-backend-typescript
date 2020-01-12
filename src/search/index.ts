874351b4153da9965b7addfdf2c1da88a1a23a5e
Dong-jin Ahn <adoji92@gmail.com>
2020-01-12 11:39:38 +0900

    Delete debugging console.log

--- src/search/index.ts
+++ src/search/index.ts
@@ -5,7 +5,6 @@ import __ from 'lodash';
 const search = new Router();
 
 search.get('/', async (ctx, next) => {
-  console.log(1);
   const { i: id, s: title } = ctx.request.query;
   if (__.isNil(id) && __.isNil(title)) {
     ctx.response.status = 400; // Not Acceptable
