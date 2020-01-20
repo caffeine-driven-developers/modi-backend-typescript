8b0772af65d055f2d1dbdee6a78c8f69d792e7db
Dong-jin Ahn <adoji92@gmail.com>
2020-01-20 21:37:55 +0900

    OMDB

--- src/search/omdb.ts
+++ src/search/omdb.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 export const searchOmdbByTitle = (title: string) => {
-  return axios.get(`https://www.omdbapi.com/?s=${title}&apikey=7ba1d8a3`);
+  return axios.get(
+    `https://www.omdbapi.com/?s=${title}&apikey=${process.env.OMDB_API_KEY}`,
+  );
 };
 
 export const searchOmdbById = (id: string) => {
-  return axios.get(`https://www.omdbapi.com/?i=${id}&apikey=7ba1d8a3`);
+  return axios.get(
+    `https://www.omdbapi.com/?i=${id}&apikey=${process.env.OMDB_API_KEY}`,
+  );
 };
