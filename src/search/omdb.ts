56802fc3d99f811a110ede30d1d1d129ab3dcd54
Dong-jin Ahn <adoji92@gmail.com>
2020-01-24 14:21:41 +0900

    Add type filter movie

--- src/search/omdb.ts
+++ src/search/omdb.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const searchOmdbByTitle = (title: string) => {
   return axios.get(
-    `https://www.omdbapi.com/?s=${title}&apikey=${process.env.OMDB_API_KEY}`,
+    `https://www.omdbapi.com/?s=${title}&type=movie&apikey=${process.env.OMDB_API_KEY}`,
   );
 };
 
 export const searchOmdbById = (id: string) => {
   return axios.get(
-    `https://www.omdbapi.com/?i=${id}&apikey=${process.env.OMDB_API_KEY}`,
+    `https://www.omdbapi.com/?i=${id}&type=movie&apikey=${process.env.OMDB_API_KEY}`,
   );
 };
