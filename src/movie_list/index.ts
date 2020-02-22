a00c2e8a79f45c28584aa66ef558943db03858dc
Dong-jin Ahn <adoji92@gmail.com>
2020-02-22 17:25:46 +0900

    Implement `POST /movie-list`

--- src/movie_list/index.ts
+++ src/movie_list/index.ts
@@ -5,7 +5,7 @@ import { getConnection } from 'typeorm';
 const movieListRouter = new Router();
 
 movieListRouter.get('/', async (ctx, next) => {
-  const conn = await getConnection();
+  const conn = getConnection();
   const movieList = new MovieList();
   movieList.title = 'hello';
   movieList.created_at = new Date();
@@ -15,6 +15,33 @@ movieListRouter.get('/', async (ctx, next) => {
   const movieListRepo = conn.getRepository(MovieList);
 
   await movieListRepo.save(movieList);
+});
+
+movieListRouter.post('/', async (ctx, next) => {
+  console.log('ctx', ctx.request.body);
+  const { title, movieIdList } = ctx.request.body;
+
+  let err;
+  try {
+    const conn = getConnection();
+    const movieList = new MovieList();
+    movieList.title = title;
+    movieList.created_at = new Date();
+    movieList.updated_at = new Date();
+    movieList.movie_id_list = movieIdList;
+
+    const movieListRepo = conn.getRepository(MovieList);
+    await movieListRepo.save(movieList);
+  } catch (error) {
+    err = error;
+  }
+
+  if (err) {
+    ctx.response.status = 400;
+    ctx.body = JSON.stringify(err);
+    return;
+  }
+  ctx.body = 'success';
 });
 
 export default movieListRouter;
