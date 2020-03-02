1ca0dbb1c9ffdea58da91bae34b70d138301db84
Dong-jin Ahn <adoji92@gmail.com>
2020-03-02 22:09:44 +0900

    Replace verbose error handling with `ctx.throw()`

--- src/search/index.ts
+++ src/search/index.ts
@@ -7,14 +7,11 @@ const searchRouter = new Router();
 searchRouter.get('/', async (ctx, next) => {
   const { i: id, s: title } = ctx.request.query;
   if (__.isNil(id) && __.isNil(title)) {
-    ctx.response.status = 400; // Not Acceptable
-    ctx.body = 'both i and t are missing';
-    return;
+    ctx.throw(400, 'bot i and t are missing');
   }
+
   if (!__.isNil(id) && !__.isNil(title)) {
-    ctx.response.status = 409; // Conflict
-    ctx.body = 'please specify either i(id) or t(title)';
-    return;
+    ctx.throw(409, 'please specify either i(id) or t(title)');
   }
 
   if (id) {
